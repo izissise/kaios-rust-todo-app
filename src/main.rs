e66a19e5d43932a63a3a97c45ee528723d29f9b6
Hugues Morisset <morisset.hugues@gmail.com>
2019-11-09 13:01:20 +0100

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,17 @@
-extern crate stdweb;
 extern crate failure;
+extern crate stdweb;
 
 use failure::Fallible;
 
 use stdweb::traits::*;
 use stdweb::unstable::TryInto;
-use stdweb::web::{
-    HtmlElement,
-    Element,
-    html_element::InputElement,
-    Node,
-    document,
-};
+use stdweb::web::{document, html_element::InputElement, Element, HtmlElement, Node};
 
-use stdweb::web::event::{
-    KeyPressEvent,
-};
+use stdweb::web::event::KeyPressEvent;
 
 // use stdweb::web::html_element::InputElement;
 
-use ::log::{info, error};
+use ::log::{error, info};
 
 mod log;
 use crate::log::init_logger;
@@ -30,15 +22,28 @@ enum DIRECTION {
 }
 
 fn get_index_of_current_selected() -> usize {
-    let selected: Element = document().query_selector("[nav-selected=true]").unwrap().unwrap();
-    match selected.get_attribute("nav-index").unwrap().parse::<usize>() {
+    let selected: Element = document()
+        .query_selector("[nav-selected=true]")
+        .unwrap()
+        .unwrap();
+    match selected
+        .get_attribute("nav-index")
+        .unwrap()
+        .parse::<usize>()
+    {
         Ok(i) => i,
-        Err(e) => { error!("{}", e); 0 },
+        Err(e) => {
+            error!("{}", e);
+            0
+        }
     }
 }
 
-fn get_all_selectable() -> impl Iterator<Item=Node> {
-    document().query_selector_all("[nav-selectable]").unwrap().iter()
+fn get_all_selectable() -> impl Iterator<Item = Node> {
+    document()
+        .query_selector_all("[nav-selectable]")
+        .unwrap()
+        .iter()
 }
 
 fn rebuild_selectable_indexes() {
@@ -53,12 +58,17 @@ fn rebuild_selectable_indexes() {
 fn focus_nth_selectable(nth: usize) {
     let first_element: HtmlElement = get_all_selectable().nth(nth).unwrap().try_into().unwrap();
     first_element.set_attribute("nav-selected", "true").unwrap();
-    first_element.set_attribute("nav-index", &nth.to_string()).unwrap();
+    first_element
+        .set_attribute("nav-index", &nth.to_string())
+        .unwrap();
     first_element.focus();
 }
 
 fn unselect_element() {
-    let selected: Element = document().query_selector("[nav-selected=true]").unwrap().unwrap();
+    let selected: Element = document()
+        .query_selector("[nav-selected=true]")
+        .unwrap()
+        .unwrap();
     selected.set_attribute("nav-selected", "false").unwrap();
 }
 
@@ -69,7 +79,8 @@ fn select_move(direction: DIRECTION) {
         DIRECTION::UP => -1,
         DIRECTION::DOWN => 1,
     };
-    let new_idx = ((current_idx as i32 + dir + all_selectable_cnt as i32) % all_selectable_cnt as i32) as usize;
+    let new_idx = ((current_idx as i32 + dir + all_selectable_cnt as i32)
+        % all_selectable_cnt as i32) as usize;
     unselect_element();
     focus_nth_selectable(new_idx);
     match new_idx {
@@ -96,9 +107,21 @@ fn toggle_todo(element: HtmlElement) {
 }
 
 fn softkey_set_label(left: &str, center: &str, right: &str) {
-    document().query_selector("#left").unwrap().unwrap().set_text_content(left);
-    document().query_selector("#center").unwrap().unwrap().set_text_content(center);
-    document().query_selector("#right").unwrap().unwrap().set_text_content(right);
+    document()
+        .query_selector("#left")
+        .unwrap()
+        .unwrap()
+        .set_text_content(left);
+    document()
+        .query_selector("#center")
+        .unwrap()
+        .unwrap()
+        .set_text_content(center);
+    document()
+        .query_selector("#right")
+        .unwrap()
+        .unwrap()
+        .set_text_content(right);
 }
 
 fn event_enter() {
@@ -108,14 +131,16 @@ fn event_enter() {
             let input: InputElement = get_all_selectable().nth(0).unwrap().try_into().unwrap();
             add_todo(input.raw_value());
             input.set_raw_value("");
-        },
+        }
         n => toggle_todo(get_all_selectable().nth(n).unwrap().try_into().unwrap()),
     }
 }
 
 fn event_soft_right() {
     let idx = get_index_of_current_selected();
-    if idx == 0 { return };
+    if idx == 0 {
+        return;
+    };
     select_move(DIRECTION::UP);
     let todo_list = document().query_selector("#toDos").unwrap().unwrap();
     let to_remove = get_all_selectable().nth(idx).unwrap();
@@ -130,15 +155,13 @@ fn main() -> Fallible<()> {
     stdweb::initialize();
     focus_nth_selectable(0);
 
-    document().add_event_listener(move |event: KeyPressEvent| {
-        match event.key().as_str() {
-            "Enter" => event_enter(),
-            "ArrowDown" => select_move(DIRECTION::DOWN),
-            "ArrowUp" => select_move(DIRECTION::UP),
-            "SoftRight" => event_soft_right(),
-            "SoftLeft" => info!("SoftLeft pressed"),
-            k => info!("Unknown key {}", k),
-        }
+    document().add_event_listener(move |event: KeyPressEvent| match event.key().as_str() {
+        "Enter" => event_enter(),
+        "ArrowDown" => select_move(DIRECTION::DOWN),
+        "ArrowUp" => select_move(DIRECTION::UP),
+        "SoftRight" => event_soft_right(),
+        "SoftLeft" => info!("SoftLeft pressed"),
+        k => info!("Unknown key {}", k),
     });
 
     stdweb::event_loop();
